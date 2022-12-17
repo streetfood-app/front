4877c46ce338f16d904e3633408f51b8e20703f9
Oleh Yavoriv <olehyavoriv@MacBook-Pro-Oleh.local>
2022-12-17 18:20:37 +0200

    Create menu list

--- src/modules/menu/components/menu-list/menu-list.component.tsx
+++ src/modules/menu/components/menu-list/menu-list.component.tsx
@@ -1,11 +1,19 @@
-import pizzaMenu from "@app/mocks/pizza.json";
 import { MenuItem } from "../menu-item/menu-item.component";
+import { Pizza } from "../../types/pizza";
+import { FC } from "react";
 
-export const MenuList = () => {
+interface MenuListProps {
+  items: Pizza[];
+}
+export const MenuList: FC<MenuListProps> = ({ items }) => {
   return (
     <div className="flex flex-wrap gap-10">
-      {pizzaMenu.map(({ image, ...pizza }) => (
-        <MenuItem {...pizza} imagePath={"/assets/pizza/" + image} />
+      {items.map(({ image, ...pizza }) => (
+        <MenuItem
+          {...pizza}
+          imagePath={"/assets/pizza/" + image}
+          key={`pizza-${pizza.id}`}
+        />
       ))}
     </div>
   );
--- src/modules/menu/components/menu-list/menu-list.story.tsx
+++ src/modules/menu/components/menu-list/menu-list.story.tsx
@@ -1,12 +1,16 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { MenuList } from "./menu-list.component";
+import pizzaMenu from "@app/mocks/pizza.json";
 
 export default {
   title: "Menu/Menu List",
   component: MenuList,
 } as ComponentMeta<typeof MenuList>;
-const Template: ComponentStory<typeof MenuList> = (args: any) => (
+const Template: ComponentStory<typeof MenuList> = (args) => (
   <MenuList {...args} />
 );
 
 export const View = Template.bind({});
+View.args = {
+  items: pizzaMenu,
+};
